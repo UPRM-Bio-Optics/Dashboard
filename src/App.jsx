02ff2de10a534dccf85bb12412548b9bc22217b0
Carlos Machin <carlos.machin@upr.edu>
2022-12-19 13:32:46 -0400

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,25 @@ import "./App.css";
 import { createTheme } from "@mui/system";
 import { green } from "@mui/material/colors";
 import { ThemeProvider } from "@emotion/react";
-import FullWidthTabs from "./components/TabPanel";
+import NavBar from "./components/NavBar";
 
 function App() {
   // Theme
   const theme = createTheme({
-    // pallete: {
-    //   primary: {
-    //     main: green[500],
-    //   },
-    //   secondary: {
-    //     main: green[500],
-    //   },
-    // },
+    pallete: {
+      primary: {
+        main: green[500],
+      },
+      secondary: {
+        main: green[500],
+      },
+    },
   });
 
   return (
     <>
       <ThemeProvider theme={theme}>
-        <FullWidthTabs theme={theme}></FullWidthTabs>
+        <NavBar></NavBar>
       </ThemeProvider>
     </>
   );
