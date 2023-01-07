e9f353b1c81344b2287191ee2da2e9677ddc81d5
Carlos Machin <carlos.machin@upr.edu>
2023-01-07 19:46:35 -0400

    auto detect browser theme

--- src/App.jsx
+++ src/App.jsx
@@ -6,48 +6,10 @@ import NavBar from "./components/NavBar";
 import "./FixImport";
 
 function App() {
-	const [scheme, setScheme] = useState("light");
-
-	// color schemes
-	// const darkScheme = {
-	// 	palette: {
-	// 		// mode: "dark",
-	// 		primary: {
-	// 			main: "#457130",
-	// 			// light: "rgb(106, 141, 89)",
-	// 			// dark: "rgb(48, 79, 33)",
-	// 			// contrastText: "#fff",
-	// 		},
-	// 		secondary: {
-	// 			main: "#b57136",
-	// 		},
-	// 		// background: {
-	// 		// 	paper: "#303030",
-	// 		// 	default: "#424242",
-	// 		// },
-	// 		// text: {
-	// 		// 	primary: "#fff",
-	// 		// 	secondary: "rgba(255, 255, 255, 0.7)",
-	// 		// 	disabled: "rgba(255, 255, 255, 0.5)",
-	// 		// 	hint: "rgba(255, 255, 255, 0.5)",
-	// 		// },
-	// 	},
-	// };
-
-	// const lightScheme = {
-	// 	palette: {
-	// 		mode: "light",
-	// 		primary: {
-	// 			main: "#0F8F46",
-	// 		},
-	// 		secondary: {
-	// 			main: "#b57136",
-	// 		},
-	// 	},
-	// };
-
-	// const theme =
-	// 	scheme === "light" ? createTheme(lightScheme) : createTheme(darkScheme);
+	// const [scheme, setScheme] = useState("light");
+	const [scheme, setScheme] = useState(
+		window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light"
+	);
 
 	const theme = createTheme({
 		palette: {
